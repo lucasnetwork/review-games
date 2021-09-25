1a0ef0df495aa5532f6ec91e63e96cc85c0bb7c1
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-09-25 18:38:10 -0300

    add typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { GameController } from './Controllers/game.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, GameController],
   providers: [AppService],
 })
 export class AppModule {}
