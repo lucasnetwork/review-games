e05b4cc07b13e2b34de52290867fde5137f4f936
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-10-09 15:09:33 -0300

    remove files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { GameController } from './Controllers/game.controller';
 import { AppService } from './app.service';
-import { Company } from './database/Entities/Company';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CompanyModules } from './Company/company.module';
-import { UserController } from './User/user.controller';
 import { UserModules } from './User/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), CompanyModules, UserModules],
-  controllers: [AppController, GameController],
+  imports: [TypeOrmModule.forRoot(), CompanyModules, AuthModule, UserModules],
   providers: [AppService],
 })
 export class AppModule {}
